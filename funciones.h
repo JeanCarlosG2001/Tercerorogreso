c0cb4a123e6e14a851a2381353b250dd5909b67c
Jean Carlos G <168456517+JeanCarlosG2001@users.noreply.github.com>
2024-06-24 11:11:48 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> 
 
-
-void ingresarCliente(char clientes[][3][40]);
-void listarPeliculas(char peliculas[][4][40]);
-void buscarporNombre(char peliculas[][4][40]);
-void buscarporGenero(char peliculas[][4][40]);
-void comprarTicket(char peliculas[][4][40], double precio[3], char clientes[][3][40], int reserva[10][4]);
-void verCompras(char peliculas[][4][40], double precio[3], char clientes[][3][40], int reserva[10][4]);
+void ingresarCliente(char clientes[5][3][40]);
+void listarPeliculas(char peliculas[10][4][40]);
+void buscarporNombre(char peliculas[10][4][40]);
+void buscarporGenero(char peliculas[10][4][40]);
+void comprarTicket(char peliculas[10][4][40], double *precio, char clientes[5][3][40], int reserva[10][4]);
+void verCompras(char peliculas[10][4][40], double *precio, char clientes[5][3][40], int reserva[10][4]);
